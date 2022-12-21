9272de47574875ef2a1f9ee94f5e16b59bc96126
Drew Manley <andrewbmanley@gmail.com>
2022-12-21 15:27:44 -0600

    finished oauth

--- pages/api/auth/[...nextauthjs].ts
+++ pages/api/auth/[...nextauthjs].ts
@@ -7,6 +7,13 @@ export default NextAuth({
     GoogleProvider({
       clientId: '58761609431-3j13utqc9del34g1kn3mklcqemdivcd5.apps.googleusercontent.com',
       clientSecret: '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',
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
     })
   ]
 })
