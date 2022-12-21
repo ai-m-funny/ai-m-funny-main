e419600c628a87b9d2466e88ecfa04b5335879a7
Brandon Miller <Brandon@MacBook-Pro.local>
2022-12-21 13:33:47 -0800

    added tests for login page

--- __tests__/login.test.tsx
+++ __tests__/login.test.tsx
@@ -1,15 +1,54 @@
+import singletonRouter, { useRouter } from 'next/router';
+import NextLink from 'next/link';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Login from '../pages/login';
 import '@testing-library/jest-dom';
 
-describe('Home', () => {
-  it('renders a heading', () => {
-    render(<Home />);
+import mockRouter from 'next-router-mock';
 
-    const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
+jest.mock('next/router', () => require('next-router-mock'));
+jest.mock('next/dist/client/router', () => require('next-router-mock'));
+
+describe('Login Page', () => {
+  beforeEach(() => {
+    mockRouter.setCurrentUrl('/login');
+    render(<Login />);
+  });
+
+  describe('Buttons', () => {
+    it('button with text: Google Sign In', () => {
+      const button = screen.getByRole('button', {
+        name: 'Sign In with Google Google Icon',
+      });
+      expect(button).toBeInTheDocument();
+    });
+  });
+
+  describe('Text', () => {
+    it('heading with text: Welcome', () => {
+      const heading = screen.getByRole('heading', {
+        name: 'Welcome',
+      });
+      expect(heading).toBeInTheDocument();
+    });
+  });
+
+  describe('Links', () => {
+    it('link with text: for Sign Up', () => {
+      const link = screen.getByRole('link', {
+        name: 'Sign Up',
+      });
+      expect(link).toBeInTheDocument();
     });
 
-    expect(heading).toBeInTheDocument();
+    it('Sign Up link routes correctly', () => {
+      fireEvent.click(
+        screen.getByRole('link', {
+          name: 'Sign Up',
+        })
+      );
+      expect(singletonRouter).toMatchObject({ asPath: '/login' });
+    });
   });
 });
